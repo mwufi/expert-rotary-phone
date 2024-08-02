35af520cd4adb94a3dc87b174a9bf5306cebc10b
Zen <ztang230@gmail.com>
2024-08-02 19:20:42 -0400

    yep

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+import createPWA from 'next-pwa'
+
+const withPWA = createPWA({
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development'
+})
+
+export default withPWA(nextConfig);
