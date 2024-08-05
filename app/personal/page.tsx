4d09da5c7f83739fb6e138822d2efcf3941c175e
Zen <ztang230@gmail.com>
2024-08-05 12:20:46 -0400

    small header

--- app/personal/page.tsx
+++ app/personal/page.tsx
@@ -20,7 +20,7 @@ const PersonalPage = () => {
 
     return (
         <div className="min-h-screen">
-            <div className="relative h-screen w-screen overflow-hidden">
+            <div className="relative h-[200px] w-screen overflow-hidden">
                 <img
                     src="https://plus.unsplash.com/premium_photo-1698578278424-295e2ce74510?q=80&w=3784&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                     alt="Background"
